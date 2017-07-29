2e96bced108bdbe850031e180eee07da7ba37803
Yunsheng Tian <knoxtian@gmail.com>
2017-07-29 12:24:46 -0700

    Add cmake configurations

---
+++ src/CMakeFiles/fluids.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for fluids.
+# This may be replaced when dependencies are built.
---
+++ src/CMakeFiles/fluids_lib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fluids_lib.dir/main.cpp.o"
+  "../lib/libfluids.pdb"
+  "../lib/libfluids.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fluids_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
